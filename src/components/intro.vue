4c2352a8937039b634cf8828411de524fb5f7f31
samantha <samantha12345587@gmail.com>
2021-01-25 00:59:34 +0800

    first commit

---
+++ src/components/intro.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.introContainer 
+</template>
+<script>
+export default {
+  name: "Intro",
+};
+</script>
+<style lang="sass">
+.introContainer
+  width: 1524px
+  height: 492px
+  border: 20px solid #ADD8E6
+  margin: auto
+  margin-top: 60px
+  background-color: #ADD8E6
+  border-radius: 123px
+  padding: 25px
+  // background-image: url("../assets/1.png")
+</style>
